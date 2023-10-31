b9afb6c4c8f2bf2d397a7840af185dfc8b439827
Hasnathasan <hasnatoooooooo@gmail.com>
2023-10-31 21:31:03 +0600

    all images added to project

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Main from './Main/Main.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Main></Main>
+  </React.StrictMode>,
+)
